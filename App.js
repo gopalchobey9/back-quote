94058a376de9074e88566a14be16d732315d3bbe
gopal chobey <100458189+gopalchobey9@users.noreply.github.com>
2023-12-31 14:06:40 +0530

    change local host

--- App.js
+++ App.js
@@ -13,13 +13,13 @@ mongoose.connect("mongodb+srv://gopalchobey9:Do23BT93oUttO0jq@cluster0.n3lohkz.m
 .catch(err => console)
  
 app.use(express.urlencoded({extended:true}));
-app.use(cors({origin: ['http://localhost:3000']}));
+app.use(cors({origin: ['https://fanciful-blini-f31419.netlify.app/']}));
 app.use(express.json());
 app.use(quoteRoute);
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://fanciful-blini-f31419.netlify.app/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
